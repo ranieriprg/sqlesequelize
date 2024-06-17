a520bcebc528a5093be761b3905fd34cfd6f12b2
ranieriprg <eddieranieri.prg@gmail.com>
2024-06-17 15:26:08 -0300

    inserido metodos crud

--- db/conn.js
+++ db/conn.js
@@ -4,11 +4,11 @@ const sequelize = new Sequelize("sqlsequelize", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
-try {
-  sequelize.authenticate();
-  console.log(`Conectado com sucesso ao banco de dados`);
-} catch (error) {
-  console.log(`Não foi possivel conectar ao banco. ${error}`);
-}
+// try {
+//   sequelize.authenticate();
+//   console.log(`Conectado com sucesso ao banco de dados`);
+// } catch (error) {
+//   console.log(`Não foi possivel conectar ao banco. ${error}`);
+// }
 
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const conn = require("./db/conn");
 const { engine } = require("express-handlebars");
+
 const app = express();
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(express.json());
-app.use(express.static("public"));
+const port = 3000;
+
+const conn = require("./db/conn");
+
+const User = require("./models/User");
 
 app.engine(
   "handlebars",
@@ -20,17 +18,90 @@ app.engine(
 );
 
 app.set("view engine", "handlebars");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
 app.use(express.static("public"));
 
-const port = 3000;
-
-app.use("/", (req, res) => {
-  res.send("hello selequelize");
+app.get("/users/create", function (req, res) {
+  res.render("adduser");
 });
 
-app.listen(port, function (err) {
-  if (err) {
-    console.log(err);
+app.post("/users/create", async function (req, res) {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  if (newsletter === "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
   }
-  console.log(`Servidor rodando na porta: ${port}`);
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect("/");
 });
+
+app.get("/users/:id", async function (req, res) {
+  const id = req.params.id;
+  const user = await User.findOne({ raw: true, where: { id: id } });
+  res.render("userview", { user });
+});
+
+app.post("/users/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await User.destroy({ where: { id: id } });
+  res.redirect("/");
+});
+
+app.get("/users/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findOne({ raw: true, where: { id: id } });
+  res.render("useredit", { user });
+});
+
+app.post("/users/update", async (req, res) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  if (newsletter === "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  const userData = {
+    id,
+    name,
+    occupation,
+    newsletter,
+  };
+
+  await User.update(userData, { where: { id: id } });
+  res.redirect("/");
+});
+
+app.get("/", async function (req, res) {
+  const users = await User.findAll({ raw: true });
+
+  console.log(users);
+  res.render("home", { users: users });
+});
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
